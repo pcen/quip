2a6af5b9a64ff409c9b29714e27c4b2cb8efafc4
Paul Cento <paul.cento@gmail.com>
2020-02-14 23:34:08 -0500

    Cite use of a 4D numpy array to compute kronecker product of a matrix with I

--- quip/math/speedy_gates.py
+++ quip/math/speedy_gates.py
@@ -1,19 +1,24 @@
 import numpy as np
 from quip.gates import identity
 
+
 def plain_kronecker(gates):
     product = np.eye(1)
     for g in gates:
         product = np.kron(product, g.matrix)
     return product
 
-def fast_kron_id(matrix, id_size):
+
+# Use a 4D array to compute matrix ⊗ I, where I is of size n
+# Divakar's stack overflow answer: https://stackoverflow.com/a/44461842
+def fast_kron_id(matrix, n):
     l, w = matrix.shape
-    p = np.zeros((l, id_size, w, id_size))
-    r = np.arange(id_size)
+    p = np.zeros((l, n, w, n))
+    r = np.arange(n)
     p[:,r,:,r] = matrix
-    p.shape = (l * id_size, w * id_size)
+    p.shape = (l * n, w * n)
     return p
+
 
 def parallel_gates_equiv(gates):
     equiv = np.eye(1)
@@ -29,6 +34,7 @@ def parallel_gates_equiv(gates):
     if c:
         equiv = fast_kron_id(equiv, 2 ** c)
     return equiv
+
 
 def benchmark():
     from timeit import default_timer as t
