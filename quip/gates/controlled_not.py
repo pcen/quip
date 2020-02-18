3a62fda09535be332057d8ccf5dfb7249edc8377
Paul Cento <paul.cento@gmail.com>
2020-02-17 23:18:53 -0500

    Adds function to determine swaps in identity for CNOT matrix

--- quip/gates/controlled_not.py
+++ quip/gates/controlled_not.py
@@ -1,15 +1,28 @@
 import numpy as np
 from quip.gates.quantum_gate import QuantumGate
 
+def b(val, len):
+    return format(val, f'0{len - 1}b')
+
 def gen_matrix(width, control, target):
+    print((1 << (width - 1)))
+    target_qb = (1 << (width - 1)) >> target
+    control_qb = (1 << (width - 1)) >> control
+
     n = width ** 2
     m = np.eye(n)
-    for i in range(n):
-        if i & (1 << control):
-            print(i)
+    swaps = [(i, i ^ target_qb) for i in range(n) if i & control_qb]
+    print(swaps)
 
     return m
 
 
 class CNOT(QuantumGate):
-    pass
+
+    symbol = 'C'
+
+    def __init__(self, control, target):
+        width = max(control, target) + 1
+        if width <= 0 or control == target:
+            del self
+        matrix = gen_matrix(width, control, target)
