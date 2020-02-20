e9e3b31783d6a0c5ca0a5ea1c1617a18b8450d3f
Paul Cento <paul.cento@gmail.com>
2020-02-19 20:02:43 -0500

    Add init files for modules containing interfaces

---
+++ quip/__init__.py
@@ -0,0 +1,3 @@
+import quip.gates
+from quip.circuit import Circuit
+from quip.quantum_system import QuantumSystem
---
+++ quip/gates/__init__.py
@@ -0,0 +1,2 @@
+from quip.gates.controlled_not import CNOT
+from quip.gates.single_qubit import *
