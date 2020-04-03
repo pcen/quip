d5c1828b451d3a1cf32e61fb1a08b8eb1ab06375
Paul Cento <paul.cento@gmail.com>
2020-04-02 23:50:05 -0400

    Implements parser
    - does not complete all code validity checks

--- quip/interpreter/gate_lookup.py
+++ quip/interpreter/gate_lookup.py
@@ -2,35 +2,36 @@ from quip.gates import *
 
 gate_table =  {
     # atomic gates
-    'hdm'  : (1, 0, hadamard,     None),
-    'plx'  : (1, 0, pauli_x,      None),
-    'ply'  : (1, 0, pauli_y,      None),
-    'plz'  : (1, 0, pauli_z,      None),
-    'phs'  : (1, 1, phase_shift,  None),
-    'fdk'  : (3, 0, fredkin,      None),
-    'tfl'  : (3, 0, toffoli,      None),
-    'cnt'  : (2, 0, CNOT,         None),
-    'swp'  : (2, 0, swap,         None),
-    'srw'  : (2, 0, sqrt_swap,    None),
+    'hdm'  : (1, 0, False, hadamard,     None),
+    'plx'  : (1, 0, False, pauli_x,      None),
+    'ply'  : (1, 0, False, pauli_y,      None),
+    'plz'  : (1, 0, False, pauli_z,      None),
+    'phs'  : (1, 1, False, phase_shift,  None),
+    'fdk'  : (3, 0, True,  fredkin,      None),
+    'tfl'  : (3, 0, True,  toffoli,      None),
+    'cnt'  : (2, 0, True,  CNOT,         None),
+    'swp'  : (2, 0, False, swap,         None),
+    'srw'  : (2, 0, False, sqrt_swap,    None),
 
     # controlled versions of single qubit gates
-    'chdm' : (2, 0, controlled_U, hadamard),
-    'cplx' : (2, 0, controlled_U, pauli_x),
-    'cply' : (2, 0, controlled_U, pauli_y),
-    'cplz' : (2, 0, controlled_U, pauli_z),
-    'cphs' : (2, 1, controlled_U, phase_shift)
+    'chdm' : (2, 0, True,  controlled_U, hadamard),
+    'cplx' : (2, 0, True,  controlled_U, pauli_x),
+    'cply' : (2, 0, True,  controlled_U, pauli_y),
+    'cplz' : (2, 0, True,  controlled_U, pauli_z),
+    'cphs' : (2, 1, True,  controlled_U, phase_shift)
 }
 
 def check_gate(instruction):
     if instruction not in gate_table:
         print(f'unknown instruction {instruction}')
         return False
+    return True
 
 def compound_gate(instruction):
     if not check_gate(instruction):
         return False
 
-    return (gate_table[instruction][3] is not None)
+    return (gate_table[instruction][4] is not None)
 
 def num_gate_params(instruction):
     if not check_gate(instruction):
@@ -43,3 +44,15 @@ def gate_width(instruction):
         return -1
 
     return gate_table[instruction][0]
+
+def is_controlled(instruction):
+    if not check_gate(instruction):
+        return False
+
+    return gate_table[instruction][2]
+
+def is_parameterized(instruction):
+    if not check_gate(instruction):
+        return False
+
+    return bool(gate_table[instruction][1])
--- quip/interpreter/quip_interpreter.py
+++ quip/interpreter/quip_interpreter.py
@@ -1,3 +1,5 @@
+from quip.interpreter.gate_lookup import *
+
 def read_file_lines(file_path):
     with open(file_path) as file:
         for line in file.readlines():
@@ -5,18 +7,52 @@ def read_file_lines(file_path):
                 continue
             yield line.strip('\n').split(';')[0]
 
-def parse_line(line):
-    line = line.split(' ')
-    instruction = line[0]
+def parse_instruction(line_start, tokens):
+    if '(' in line_start and ')' in line_start:
+        tokens['instruction'] = line_start.split('(')[0]
+        value = line_start.split('(')[-1].split(')')[0].strip()
+        try:
+           tokens['params'] = [float(value)]
+        except e:
+            print(f'invalid gate parameter {value}')
+    else:
+        tokens['instruction'] = line_start
+
+    check_gate(tokens['instruction'])
+
+def parse_qubits(line, tokens):
+    if len(line) < gate_width(tokens['instruction']):
+        print(f'missing qubits for {tokens["instruction"]}')
+
     qubits = []
-    for q in line[1:]:
-        if not q.strip():
+    controlled = 0
+    if is_controlled(tokens['instruction']):
+        controlled = 1
+        if not line[0].strip() or not line[0].strip().isdecimal():
+            print(f'invalid control qubit')
+        tokens['control'] = int(line[0].strip())
+
+    for i in range(controlled, len(line)):
+        if not line[i].strip():
             continue
-        elif q.isdecimal():
-            qubits.append(int(q))
-        else:
-            print(f'invalid value: {q}')
-    return instruction, qubits
+        elif line[i].strip().isdecimal():
+            qubits.append(int(line[i].strip()))
+
+    tokens['qubits'] = qubits
+    if len(qubits) + controlled != gate_width(tokens['instruction']):
+        print(f'wrong number of qubits for {tokens["instruction"]}')
+
+def parse_line(line):
+    tokens = {}
+    params = []
+
+    line = line.split(' ')
+
+    parse_instruction(line[0], tokens)
+    parse_qubits(line[1:], tokens)
+    tokens['controlled'] = is_controlled(tokens['instruction'])
+    tokens['parameterized'] = is_parameterized(tokens['instruction'])
+    return tokens
 
 def tokenize(code):
     tokenized = []
@@ -24,9 +60,5 @@ def tokenize(code):
         tokenized.append(parse_line(line))
     return tokenized
 
-prog = "C:\cygwin64\home\Paul\Dev\quip\prog.q"
-
-program = tokenize(prog)
-
-for p in program:
-    print(p)
+def generate_circuit(parsed_code):
+    pass
